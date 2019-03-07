6b985452c3320c152978ea9f88df80c43b4dcbc1
edreeseg <edward.reeseg@gmail.com>
2019-03-07 10:53:25 -0500

    Adding dotenv, root endpoint to direct to correct endpoint.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import your node modules
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const server = express();
@@ -10,6 +11,9 @@ const port = process.env.PORT || 5000;
 const postRoutes = require('./routes/postRoutes');
 
 server.use('/api/posts', postRoutes);
+server.get('/', (req, res) => {
+  res.send('Please direct requests to /api/posts endpoint.');
+});
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
